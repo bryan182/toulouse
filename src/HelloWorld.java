0e5606e29d93a1cc0abd7b2bbcb23eadee0d0d81
charles.thiebaud <charles.thiebaud@laposte.net>
2016-09-14 08:50:17 +0200

    change to class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,16 @@
 public class HelloWorld {
 public static void main(String[] args) {
+
+	HelloWorld hello = new HelloWorld();
+	hello.sayHello();
+}
+
+public void sayHello()
+{
 	System.out.println("Hello World");
 }
+<<<<<<< HEAD
 }
+=======
 //coucou
+>>>>>>> 9eaee18036c6aaf5fb10dffd79b55abfcb06a12d
